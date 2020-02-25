28e5901558c85b6468d319d7ff46a41abe01672a
Guilherme Rodrigues <guilhermersl@yahoo.com.br>
2020-02-25 10:43:13 -0300

    Teste Heroku - Deploy v1

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "TESTE!!!!!!!!!!!!";
-?>
+<?php  
+	define("BASE_URL", "http://localhost/projetos/desafiozeedog/"); 
+	define("BASE_URL_SRC", "http://localhost/projetos/desafiozeedog/src/");
+
+	require '/src/controller.php';
+	require '/src/apigithub.php';
+	echo "até aqui ok"
+	/*
+	//CHAMA CONTROLLER QUE REALIZARÁ A REQUISIÇÃO PARA A CARGA INICIAL
+	$api = new APIGitHub();
+	$controller = new Controller($api);
+	$dados = $controller->run();
+	
+	extract($dados);
+	require '/src/views/home.php';
+	*/
+
+?>     
